1fb9c39501a72fd4e12a9efc95145d877dd84271
emanuel-frs <efartecontato@gmail.com>
2025-04-04 06:56:51 -0300

    Test Commit

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { SidebarProvider } from './context/SidebarContext';
 import { useEffect } from 'react';
 
 export default function App() {
-  useEffect(() => {
+  //useEffect(() => {
     useFonts({
       'Baloo2-Regular': require('./assets/fonts/Baloo2-Regular.ttf'),
       'Baloo2-Medium': require('./assets/fonts/Baloo2-Medium.ttf'),
@@ -16,7 +16,7 @@ export default function App() {
       'Baloo2-Bold': require('./assets/fonts/Baloo2-Bold.ttf'),
       'Baloo2-ExtraBold': require('./assets/fonts/Baloo2-ExtraBold.ttf'),
     });
-  },[])
+  //},[])
 
   return (
     <SidebarProvider>
